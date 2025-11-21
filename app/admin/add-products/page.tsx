71a6b07aadba154d78f322ebfbf431b325e63a27
48pz <huanmiaoi@outlook.com>
2025-11-21 15:19:01 +1000

    add product

---
+++ app/admin/add-products/page.tsx
@@ -0,0 +1,28 @@
+import Container from "@/app/components/Container";
+import FormWrap from "@/app/components/FormWrap";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/products/NullData";
+import AddProductForm from "./AddProductForm";
+
+
+const AddProducts = async () => {
+
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser || currentUser.role !== 'ADMIN') {
+        return <NullData title="Oops! Access denied"/>
+    }
+
+    return (
+        <div className="p-8">Add Products
+            <Container>
+                <FormWrap>
+                    <AddProductForm/>
+                </FormWrap>
+            </Container>
+
+        </div>
+    );
+}
+
+export default AddProducts;
---
+++ app/components/admin/AdminNav.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { Link } from "@mui/material";
+import { MdDashboard, MdDns, MdFormatListBulleted, MdLibraryAdd } from "react-icons/md";
+import AdminNavItem from "./AdminNavItem";
+import { usePathname } from "next/navigation";
+import Container from "../Container";
+
+const AdminNav = () => {
+    //nextjs hook it returns current url
+    const pathname = usePathname()
+    return (
+        <div className="w-full shadow-sm top-20 border-b-[1px] pt-4">
+            <Container>
+                <div className="flex flex-row items-center justify-between md:justify-center gap-8 md:gap-12 overflow-x-auto flex-nowrap">
+                    <Link href='/admin' className="!no-underline">
+                        <AdminNavItem lable="Summary" icon={MdDashboard} selected={pathname === "/admin"} />
+                    </Link>
+                    <Link href='/admin/add-products' className="!no-underline">
+                        <AdminNavItem lable="AddProducts" icon={MdLibraryAdd} selected={pathname === "/admin/add-products"} />
+                    </Link>
+                    <Link href='/admin/manage-products' className="!no-underline">
+                        <AdminNavItem lable="ManageProducts" icon={MdDns} selected={pathname === "/admin/manage-products"} />
+                    </Link>
+                    <Link href='/admin/manage-orders' className="!no-underline">
+                        <AdminNavItem lable="ManageOrders" icon={MdFormatListBulleted} selected={pathname === "/admin/manage-orders"} />
+                    </Link>
+                </div>
+            </Container>
+        </div>
+    );
+}
+
+export default AdminNav;
