5a60621a4d0215279512796bfb24b09e2afe7dcf
48pz <huanmiaoi@outlook.com>
2025-11-22 21:21:39 +1000

    manage products part1

--- app/admin/manage-products/page.tsx
+++ app/admin/manage-products/page.tsx
@@ -1,5 +1,24 @@
-const ManageProducts = () => {
-    return ( <div>Manage Products</div> );
-}
- 
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import getProducts from "@/actions/getProducts";
+import Container from "@/app/components/Container";
+import ManageProductsClient from "./ManageProductsClient";
+import NullData from "@/app/components/products/NullData";
+
+const ManageProducts = async () => {
+  const products = await getProducts({ category: null });
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title={"Oops! Access denied"} />;
+  }
+
+  return (
+    <div className="pt-8">
+      <Container>
+        <ManageProductsClient products={products} />
+      </Container>
+    </div>
+  );
+};
+
 export default ManageProducts;
